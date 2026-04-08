61aafffe832de5fcb2fb42b8df816f644e8245ff
codelabuk <afzal232@gmail.com>
2026-04-08 23:24:43 +0530

    feat [Spark-k8s-flow] : Read datasource from Minio bucket

--- jobs/scala/scalasets-job/src/main/scala/org/codelabuk/MainSparkLabuk.scala
+++ jobs/scala/scalasets-job/src/main/scala/org/codelabuk/MainSparkLabuk.scala
@@ -5,14 +5,11 @@ import org.apache.spark.sql.SparkSession
 object MainSparkLabuk {
 
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("Word Count").getOrCreate()
-    val sc = spark.sparkContext
-    val txtData = sc.parallelize(List("Hello Spark", "Hello Scala", "Hello Airflow", "Hello Docker", "Hello CodeLabuk"))
-    val counts = txtData.flatMap(line => line.split(" "))
-      .map(word => (word, 1))
-      .reduceByKey((a, b) => a+ b)
-
-    counts.collect().foreach(println)
+    val spark = SparkSession.builder().appName("Sellers-City").getOrCreate()
+    val df = spark.read.option("header","true")
+      .csv("s3a://data/csv/sellers/")
+    df.printSchema()
+    df.show(10)
   }
 
 }
